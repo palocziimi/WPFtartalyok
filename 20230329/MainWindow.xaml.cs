60511b620cf6ddd0bb2ca4d731697b23d345ee73
CAD2-06 <you@example.com>
2023-03-29 12:21:17 +0200

    masodik

--- 20230329/MainWindow.xaml.cs
+++ 20230329/MainWindow.xaml.cs
@@ -39,14 +39,19 @@ namespace _20230329
             txtAel.Text = txtBel.Text = txtCel.Text = "10";
         }
 
-        private void btnFelvesz_Click(object sender, RoutedEventArgs e)
+        private void ListUpdate()
         {
-            list.Add(new Tartaly(txtNev.Text, Convert.ToInt32(txtAel.Text), Convert.ToInt32(txtBel.Text), Convert.ToInt32(txtCel.Text)));
             lbTartalyok.Items.Clear();
             foreach (Tartaly item in list)
             {
                 lbTartalyok.Items.Add(item.Info());
             }
+        }
+
+        private void btnFelvesz_Click(object sender, RoutedEventArgs e)
+        {
+            list.Add(new Tartaly(txtNev.Text, Convert.ToInt32(txtAel.Text), Convert.ToInt32(txtBel.Text), Convert.ToInt32(txtCel.Text)));
+            ListUpdate();
         }
 
         private void btnRogzit_Click(object sender, RoutedEventArgs e)
@@ -61,57 +66,60 @@ namespace _20230329
             sw.Close();
             MessageBox.Show("Sikeres fájlbaírás!");
         }
-    }
 
-    class Tartaly
-    {
-        private String nev;
-        private int a, b, c;
-        private double aktLiter;
-
-        public Tartaly(String nev, int a, int b, int c)
+        private bool CheckSelectedItem()
         {
-            this.nev = nev;
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            aktLiter = 0;
-        }
-
-        public Tartaly(String nev)
-        {
-            this.nev = nev;
-            this.a = 10;
-            this.b = 10;
-            this.c = 10;
-            aktLiter = 0;
-        }
-        public double Terfogat { get => a * b * c; }
-        public double Toltottseg { get => aktLiter / (Terfogat / 1000) * 100; }
-        public string Nev { get => nev; }
-        public int AEl { get => a; }
-        public int BEl { get => b; }
-        public int CEl { get => c; }
-        public double AktLiter { get => aktLiter; }
-
-        public void Tolt(double liter)
-        {
-            if (Terfogat * 1000 < liter + aktLiter)
+            if (lbTartalyok.SelectedIndex < 0)
             {
-                throw new OverflowException("Sok lesz!");
-                return;
+                MessageBox.Show("Nem lett kiválasztva elem!");
+                return false;
             }
-            aktLiter += liter;
+            return true;
         }
-        public void DuplazMeretet() => a *= 2;
-        public void TeljesLeengedes() => aktLiter = 0;
-
-        public string Info()
+        
+        private void btnDuplaz_Click(object sender, RoutedEventArgs e)
         {
-            return $"{this.nev}: {this.Terfogat * 1000:n1} cm3 = ({this.Terfogat:n2} liter)," +
-                $" töltöttsége: {this.Toltottseg:n2}%, ({this.aktLiter:n2} liter)," +
-                $" méretei: {this.a}x{this.b}x{this.c} cm";
+            if (CheckSelectedItem())
+            {
+                list[lbTartalyok.SelectedIndex].DuplazMeretet();
+                ListUpdate();
+            }
+        }
 
+        private void btnLeenged_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckSelectedItem())
+            {
+                list[lbTartalyok.SelectedIndex].TeljesLeengedes();
+                ListUpdate();
+            }
+        }
+
+        private void btntolt_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckSelectedItem())
+            {
+                int index = lbTartalyok.SelectedIndex;
+                try
+                {
+                    double toCharge = Convert.ToDouble(txtMennyitTolt.Text);
+                    list[lbTartalyok.SelectedIndex].Tolt(toCharge);
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Túl nagy szám!");
+                    return;
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Nem szám lett megadva!");
+                    return;
+                }
+                ListUpdate();
+                lbTartalyok.Focus();
+            }
         }
     }
+
+    
 }
---
+++ 20230329/Tartaly.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230329
+{
+    class Tartaly
+    {
+        private String nev;
+        private int a, b, c;
+        private double aktLiter;
+
+        public Tartaly(String nev, int a, int b, int c)
+        {
+            this.nev = nev;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            aktLiter = 0;
+        }
+
+        public Tartaly(String nev)
+        {
+            this.nev = nev;
+            this.a = 10;
+            this.b = 10;
+            this.c = 10;
+            aktLiter = 0;
+        }
+        public double Terfogat { get => a * b * c; }
+        public double Toltottseg { get => aktLiter * 1000 / Terfogat / 10; }
+        public string Nev { get => nev; }
+        public int AEl { get => a; }
+        public int BEl { get => b; }
+        public int CEl { get => c; }
+        public double AktLiter { get => aktLiter; }
+
+        public void Tolt(double liter)
+        {
+            if (Terfogat / 1000 < liter + aktLiter)
+            {
+                throw new OverflowException("Sok lesz!");
+                return;
+            }
+            aktLiter += liter;
+        }
+        public void DuplazMeretet() => a *= 2;
+        public void TeljesLeengedes() => aktLiter = 0;
+
+        public string Info()
+        {
+            return $"{this.nev}: {this.Terfogat * 1000:n1} cm3 = ({this.Terfogat:n2} liter)," +
+                $" töltöttsége: {this.Toltottseg:n2}%, ({this.aktLiter:n2} liter)," +
+                $" méretei: {this.a}x{this.b}x{this.c} cm";
+
+        }
+    }
+}
